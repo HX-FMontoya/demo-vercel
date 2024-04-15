f787f5c68df4640fdcdd3fdf544b35df564c0add
HX-FMontoya <felipeciromontoya.developer@gmail.com>
2024-04-15 12:59:16 -0500

    update: env example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center bg-indigo-300 text-stone-700 p-8 m-16">
       <h1>
-        Hola desde la demo de vercel
+        Hola desde la demo de vercelllllll
       </h1>
       <p className="bg-slate-500 text-white rounded-xl p-2">{process.env.NEXT_PUBLIC_MAIL_EXAMPLE}</p>
     </div>
