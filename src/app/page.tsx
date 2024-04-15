14d7d7ff1c6cc9acd269e33704f74c16490706ed
HX-FMontoya <felipeciromontoya.developer@gmail.com>
2024-04-15 12:29:03 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center bg-indigo-300 text-stone-700 p-8 m-16">
+      <h1>Hola desde la demo de vercel</h1>
+    </div>
+  );
+}
